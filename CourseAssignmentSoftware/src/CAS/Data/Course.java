899b9da115811a7827f2fa8daf9ba62d2bc29480
SJBenKiki <tomtom1113@gmail.com>
2013-12-05 21:43:52 -0500

    Course class stub
    This is a stub per the design document., please let me know if I need to
    change/add anything

--- CourseAssignmentSoftware/src/CAS/Data/Course.java
+++ CourseAssignmentSoftware/src/CAS/Data/Course.java
@@ -2,5 +2,32 @@ package CAS.Data;
 
 public class Course
 {
-
+    private int id;
+    private String workarea;
+    private Instructor instructor;
+    private Day[] day;
+    private Time start;
+    private Time end;
+    private String subject;
+    private int number;
+    private String section;
+    private int session;
+    private String title;
+    private int credits;
+    private String campus;
+    private String room;
+       
+    public Course(){
+       // todo complete constructor  
+    }
+    
+    public int hashCode(){
+        // todo
+        return 0;
+    }
+    
+    public boolean equals(Object object){
+        // todo
+        return true;
+    }
 }
