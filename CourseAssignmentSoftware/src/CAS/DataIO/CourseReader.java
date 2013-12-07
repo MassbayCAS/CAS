d3ece7b015dc6e52d2cb10de4e85a2902f1dd3d4
Justincakes <j_jiang@post.massbay.edu>
2013-12-07 17:07:12 -0500

    CourseReader update (not tested yet!) - main issue will be making sure I counted the number of tabs correctly when I tokenized. - any variables from Course class that are not in the Course Schedule file I set to null or zero. I'm assuming there is another class that updates them later.

--- CourseAssignmentSoftware/src/CAS/DataIO/CourseReader.java
+++ CourseAssignmentSoftware/src/CAS/DataIO/CourseReader.java
@@ -13,6 +13,9 @@ package CAS.DataIO;
 import java.io.*;
 import java.util.HashMap;
 import CAS.Data.Course;
+import CAS.Data.Day;
+import CAS.Data.Time;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -23,17 +26,66 @@ public class CourseReader extends FileReader
         super(filename);
     }
     
-    public HashMap<String, Course> loadCourses()
+    public HashMap<String, Course> loadCourses() throws FileNotFoundException
     {
         Scanner scan = new Scanner(getFile());
         HashMap<String, Course> courseList = new HashMap<String, Course>();
+        scan.nextLine();
         while(scan.hasNext())
         {
             String line = scan.nextLine();
+            String[] splitline = line.split("\\t");
+            
+            int id = Integer.parseInt(splitline[0]);
+            String workArea = null;
+            ArrayList<Day> days = new ArrayList<Day>();
+            if(splitline[5].contains("M"))
+                days.add(Day.MONDAY);
+            if(splitline[5].contains("T"))
+                days.add(Day.TUESDAY);
+            if(splitline[5].contains("W"))
+                days.add(Day.WEDNESDAY);
+            if(splitline[5].contains("R"))
+                days.add(Day.THURSDAY);
+            if(splitline[5].contains("F"))
+                days.add(Day.FRIDAY);
+            if(splitline[5].contains("Sa"))
+                days.add(Day.SATURDAY);
+            if(splitline[5].contains("Su"))
+                days.add(Day.SUNDAY);
+            
+            String[] startTime = splitline[6].split(":");
+            int hour = Integer.parseInt(startTime[0].trim());
+            char[] startMin = startTime[1].toCharArray();
+            String minString = String.valueOf(startMin[0]) + String.valueOf(startMin[1]);
+            int min = Integer.parseInt(minString);
+            if(startMin[2] == 'p')
+                hour += 12;
+            Time start = new Time(hour, min);
+            String[] endTime = splitline[7].split(":");
+            hour = Integer.parseInt(endTime[0].trim());
+            char[] endMin = endTime[1].toCharArray();
+            minString = String.valueOf(endMin[0]) + String.valueOf(endMin[1]);
+            min = Integer.parseInt(minString);
+            if(endMin[2] == 'p')
+                hour += 12;
+            Time end = new Time(hour, min);
+            
+            String[] subjectNumber = splitline[1].split("\\s");
+            String subject = subjectNumber[0];
+            int number = Integer.parseInt(subjectNumber[1]);
+            String section = splitline[2];
+            int session = 0;
+            String title = splitline[3];
+            int credits = 0;
+            String campus = splitline[4];
+            String room = null;
+            
             // Waiting on Course class to be finished to finalize this part
-            Course course = new Course(tba);
+            Course course = new Course(id, workArea, days, start, end, subject, 
+                    number, section, session, title, credits, campus, room);
             String key = course.getSubject() + course.getNumber() + course.getSection();
-            courseList.add(key, course);
+            courseList.put(key, course);
         }
         return courseList;
     }
