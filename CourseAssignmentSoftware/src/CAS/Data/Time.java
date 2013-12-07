2878d635a1fde60465002fbc2a7b4a4ed09cb0ba
SJBenKiki <tomtom1113@gmail.com>
2013-12-06 22:25:15 -0500

    forgot to get rid of sec format

--- CourseAssignmentSoftware/src/CAS/Data/Time.java
+++ CourseAssignmentSoftware/src/CAS/Data/Time.java
@@ -13,13 +13,13 @@ public class Time{
      min = ((m >= 0 && m < 60) ? m :0);
 }
  
-// convert to String in universal-time format (HH:MM:SS)
+// convert to String in universal-time format (HH:MM)
  public String toUniversalString(){
-     return String.format( "%02d:%02d:%02d", hour, min);
+     return String.format( "%02d:%02", hour, min);
  }
     
- // convert to String in standard-time format (H:MM:SS AM PM)
+ // convert to String in standard-time format (H:MM AM PM)
  public String toString(){ 
-     return String.format( "%d:%02d:%02d %s",(( hour == 0 || hour == 12) ? 12 : hour %12),min,(hour< 12?"AM":"PM"));
+     return String.format( "%d:%02%s",(( hour == 0 || hour == 12) ? 12 : hour % 12),min,(hour< 12?"AM":"PM"));
  }
 }
