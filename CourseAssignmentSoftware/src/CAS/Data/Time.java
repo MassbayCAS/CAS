1b01eeb15c1992f59367541b3d637a4e7aba3005
SJBenKiki <tomtom1113@gmail.com>
2013-12-06 22:00:01 -0500

    Changed format of Time class, entered validations and string format

--- CourseAssignmentSoftware/src/CAS/Data/Time.java
+++ CourseAssignmentSoftware/src/CAS/Data/Time.java
@@ -4,38 +4,22 @@ package CAS.Data;
 public class Time{
  private int hour;
  private int min;
- private int sec;
 
- public Time(){
-
- }
-
- public int getHour() {
-  return hour;
- }
-
- public void setHour(int hour) {
-  this.hour = hour;
- }
-
- public int getMin() {
-  return min;
- }
-
- public void setMin(int min) {
-  this.min = min;
- }
-
- public int getSec() {
-  return sec;
- }
-
- public void setSec(int sec) {
-  this.sec = sec;
+ public Time(int hour, int min){
  }
  
- public String toString(){
-  return getHour() + ":" + getMin() + ":" + getSec();
- }
+ public void setTime(int h, int m){
+     hour =((h >= 0&& h < 24) ? h :0);
+     min = ((m >= 0 && m < 60) ? m :0);
+}
  
+// convert to String in universal-time format (HH:MM:SS)
+ public String toUniversalString(){
+     return String.format( "%02d:%02d:%02d", hour, min);
+ }
+    
+ // convert to String in standard-time format (H:MM:SS AM PM)
+ public String toString(){ 
+     return String.format( "%d:%02d:%02d %s",(( hour == 0 || hour == 12) ? 12 : hour %12),min,(hour< 12?"AM":"PM"));
+ }
 }
