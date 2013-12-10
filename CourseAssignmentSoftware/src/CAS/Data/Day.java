a06eef0223f47b6006b80592425404dc50b1e7c8
jkuk <j_kuk@post.massbay.edu>
2013-12-09 20:36:53 -0500

    implemented new course class, fixed course reader

--- CourseAssignmentSoftware/src/CAS/Data/Day.java
+++ CourseAssignmentSoftware/src/CAS/Data/Day.java
@@ -1,6 +1,19 @@
-
 package CAS.Data;
 
+public enum Day {SUNDAY,   MONDAY, TUESDAY, WEDNESDAY,
+	THURSDAY, FRIDAY, SATURDAY;
 
-public enum Day {SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
-    THURSDAY, FRIDAY, SATURDAY};
+	@Override
+	public String toString() {
+		switch (this) {
+		case SUNDAY: return "Su";
+		case MONDAY: return "M";
+		case TUESDAY: return "T";
+		case WEDNESDAY: return "W";
+		case THURSDAY: return "R";
+		case FRIDAY: return "F";
+		case SATURDAY: return "Sa";
+		default: throw new IllegalArgumentException();
+		}
+	}
+}
--- CourseAssignmentSoftware/src/CAS/Data/Time.java
+++ CourseAssignmentSoftware/src/CAS/Data/Time.java
@@ -1,30 +1,28 @@
-
 package CAS.Data;
 
 public class Time{
- private int hour;
- private int min;
+	private int hour;
+	private int min;
 
- public Time(int hour, int min){
-     setTime(hour, min);
- }
- 
- public void setTime(int h, int m){
-     hour =((h >= 0&& h < 24) ? h :0);
-     min = ((m >= 0 && m < 60) ? m :0);
-}
- 
- public int getHour() {
-     return hour;
- }
- 
-// convert to String in universal-time format (HH:MM)
- public String toUniversalString(){
-     return String.format( "%02d:%02", hour, min);
- }
-    
- // convert to String in standard-time format (H:MM AM PM)
- public String toString(){ 
-     return String.format( "%d:%02%s",(( hour == 0 || hour == 12) ? 12 : hour % 12),min,(hour< 12?"AM":"PM"));
- }
+	public Time(int hour, int min) {
+		if(hour < 0 || hour > 23 || min < 0 || min > 59) {
+			throw new IllegalArgumentException();
+		}
+		this.hour = hour;
+		this.min = min;
+	}
+
+	public int getHour() {
+		return hour;
+	}
+
+	// convert to String in universal-time format (HH:MM)
+	public String toUniversalString(){
+		return String.format( "%02d:%02d", hour, min);
+	}
+
+	// convert to String in standard-time format (H:MM AM PM)
+	public String toString(){ 
+		return String.format( "%d:%02d",(( hour == 0 || hour == 12) ? 12 : hour % 12),min,(hour< 12?"AM":"PM"));
+	}
 }
