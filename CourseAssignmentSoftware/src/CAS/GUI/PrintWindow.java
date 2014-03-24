ff50aef07361c910506ca415de68ed67e86828d8
Owner <Owner@192.168.44.103>
2014-03-24 11:50:27 -0400

    Allowed the user to declare their own file extension.

--- CourseAssignmentSoftware/src/CAS/GUI/PrintWindow.java
+++ CourseAssignmentSoftware/src/CAS/GUI/PrintWindow.java
@@ -84,8 +84,13 @@ public class PrintWindow extends JFrame
                 int choice = fileChooser.showSaveDialog(PrintWindow.this);
                 if (choice == JFileChooser.APPROVE_OPTION) {
                     File file = fileChooser.getSelectedFile();
-                    String filename = file.getPath() + ".doc";
-                    BufferedWriter bfWrite = null;
+                    String filename;
+                    CharSequence cs = ".";
+                    if (file.getPath().contains(cs)) {
+                        filename = file.getPath();
+                    } else {
+                        filename = file.getPath() + ".doc";
+                    } BufferedWriter bfWrite = null;
                     try {
                         bfWrite = new BufferedWriter(new FileWriter(filename));
                         bfWrite.write(textArea.getText());
