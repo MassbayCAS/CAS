2c847c523eb4ca1007805c2abeb225c84d1b17a4
Audi <Audi@192.168.54.144>
2014-04-02 18:43:00 +0200

    Removed Login  Window for testing

--- CourseAssignmentSoftware/src/CAS/CourseAssignmentSoftware.java
+++ CourseAssignmentSoftware/src/CAS/CourseAssignmentSoftware.java
@@ -1,11 +1,13 @@
 package CAS;
 
+import CAS.GUI.FileSelectorWindow;
 import CAS.GUI.Login.LoginFrame;
 
 public class CourseAssignmentSoftware {
 
     public static void main(String[] args) {
-        new LoginFrame();
+        //new LoginFrame();
+        new FileSelectorWindow();
     }
     
 }
